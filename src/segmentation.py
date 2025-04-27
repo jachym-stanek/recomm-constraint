c8cdb8f9f91f32df11e2f5557a3485501264a483
Jachym Stanek <jachym.stanek@recombee.com>
2025-04-27 08:28:34 +0200

    Improved segmentation and fixed overwriting segments where different properties have the same value

--- src/segmentation.py
+++ src/segmentation.py
@@ -65,8 +65,23 @@ class SegmentationExtractor:
                 items.append(row)
         return items
 
-    # put item indexes into corresponding segments
-    def extract_segments(self, segmentation_property):
+    def extract_segments(self, segmentation_properties):
+        """
+        Extract segments from the dataset based on the specified segmentation properties.
+        :param segmentation_properties: str or list of str, the property or properties to segment by
+        :return: dict<seg_id, Segment>
+        """
+        if isinstance(segmentation_properties, str):
+            segmentation_properties = [segmentation_properties]
+
+        self.segments = {}
+        for segmentation_property in segmentation_properties:
+            segments = self._extract_segments_for_property(segmentation_property)
+            self.segments.update(segments) # if segnent id is the same across different properties, it will be overwritten -> TODO: fix
+
+        return self.segments
+
+    def _extract_segments_for_property(self, segmentation_property):
         if segmentation_property not in self.item_properties:
             raise ValueError(f"Property '{segmentation_property}' is not a valid item property.")
 
@@ -80,17 +95,19 @@ class SegmentationExtractor:
             if isinstance(property_value, list):
                 # Add item to each segment corresponding to the list elements
                 for value in property_value:
-                    if value not in segments:
-                        segments[value] = Segment(segment_id=value, segmentation_property=segmentation_property)
-                    segments[value].add(item_idx)
+                    segment_value_id = str(value) + '-' + segmentation_property # needed to make unigue segment identifier
+                    if segment_value_id not in segments:
+                        segments[segment_value_id] = Segment(segment_id=value, segmentation_property=segmentation_property)
+                    segments[segment_value_id].add(item_idx)
             else:
                 # Add item to the segment for the single value
-                if property_value not in segments:
-                    segments[property_value] = Segment(segment_id=property_value,
+                segment_value_id = str(property_value) + '-' + segmentation_property
+                if segment_value_id not in segments:
+                    segments[segment_value_id] = Segment(segment_id=property_value,
                                                        segmentation_property=segmentation_property)
-                segments[property_value].add(item_idx)
+                segments[segment_value_id].add(item_idx)
 
-        self.segments = segments
+        return segments
 
     def get_segments(self):
         """
@@ -99,38 +116,60 @@ class SegmentationExtractor:
         """
         return self.segments
 
-    def get_segments_for_recomms(self, recomms):
-        recomms_segments = []
-        for segment in self.segments:
+    def get_segments_dict_for_recomms(self, recomms, translate_idx2id=False):
+        recomms_segments = {}
+
+        if translate_idx2id:
+            items = [self.item_idx2id.get(str(idx)) for idx in recomms]
+        else:
+            items = recomms
+
+        for segment in self.segments.values():
             segment_items = []
-            for item in recomms:
+            for item in items:
                 if item in segment:
                     segment_items.append(item)
             if len(segment_items) > 0:
                 # create new segment with only the items that are in the recommendations and using item rating matrix index instead of item id
-                recomms_segments.append(Segment(segment.segment_id, segment.segmentation_property, *segment_items))
+                recomms_segments[segment.segment_id] = (Segment(segment.segment_id, segment.segmentation_property, *segment_items))
+        return recomms_segments
+
+    def get_segments_for_recomms(self, recomms, translate_idx2id=False):
+        recomms_segments = {}
+
+        if translate_idx2id:
+            items = [self.item_idx2id.get(str(idx)) for idx in recomms]
+        else:
+            items = recomms
+
+        for item in items:
+            item_segments = []
+            for segment in self.segments.values():
+                if item in segment:
+                    item_segments.append(segment)
+            recomms_segments[item] = item_segments
+
         return recomms_segments
 
 
-def test_segmentation_extractor():
+if __name__ == "__main__":
     # Usage Example
     settings = Settings()
+    settings.set_dataset_in_use("industrial_dataset1")
 
     extractor = SegmentationExtractor(settings)
-    segments = extractor.extract_segments('genres')
+    segments = extractor.extract_segments(['category_1', 'category_2', 'category_3'])
 
-    for segment in segments:
-        print(segment)
+    print(f"Extracted {len(segments)} segments.")
 
     # test get_segments_for_recomms
     print("Testing get_segments_for_recomms")
     recomms = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    recomms_segments_dict = extractor.get_segments_dict_for_recomms(recomms)
+    for segment in recomms_segments_dict:
+        print(f"Segment: {segment} has items: {[item for item in recomms_segments_dict[segment]]}")
+    assert len(recomms_segments_dict) > 0
+
     recomms_segments = extractor.get_segments_for_recomms(recomms)
-    for segment in recomms_segments:
-        print(segment)
-        print(list(segment))
-    assert len(recomms_segments) > 0
-
-
-if __name__ == "__main__":
-    test_segmentation_extractor()
+    for item in recomms_segments:
+        print(f"Item: {item} has segments: {[seg.id for seg in recomms_segments[item]]}")
